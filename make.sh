0004275858e49e5a84fed89d02450502d33bce3a
ViniciusPerillo <vinicius.perillo25@gmail.com>
2025-07-18 22:12:19 -0300

    fix: make.sh

--- make.sh
+++ make.sh
@@ -3,9 +3,17 @@
 mkdir ./config
 mkdir ./data_summary
 
-python -m venv kanban-temp
-source ./kanban/bin/activate
-pip install -r requirements.py
-rm -r ./kanban-temp
+cd ./data_summary/
+touch stock_data.txt
+chmod a+rwx stock_data.txt
+touch factory_data.txt
+chmod a+rwx factory_data.txt
+touch product_data.txt
+chmod a+rwx product_data.txt
+cd ..
 
+python3 -m venv kanban-temp
+source ./kanban-temp/bin/activate
+pip install -r requirements.txt
 python3 generate_config.py
+rm -r ./kanban-temp
